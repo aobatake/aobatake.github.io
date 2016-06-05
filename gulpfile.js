88fa57f5e947ba210a6f3097c11a0b491d689a2a
Andrew Obatake <aobatake@hawaii.edu>
2016-06-05 00:39:22 -1000

    updated home page, gulpfile processes css

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,67 @@
 var gulp = require('gulp');
+
+//html
 var nunjucksRender = require('gulp-nunjucks-render');
+
+//dev
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
-var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+
+//sass
+var sass = require('gulp-sass');
+
+//images
 var imagemin = require('gulp-imagemin');
+
+//js
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+
+//ghpages
 var deploy = require("gulp-gh-pages");
 
+//css
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 
+
+gulp.task('default', ['serve','image']);
+
+
+//Post Process CSS
+gulp.task('css', function () {
+    var processors = [
+        autoprefixer({browsers: ['last 1 version']}),
+        cssnano(),
+    ];
+    return gulp.src('src/css/*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+//Deploy to gh-pages
 gulp.task('deploy', function(){
 	return gulp.src("./dist/**/*")
 		.pipe(deploy())
 });
 
-gulp.task('serve',['sass','html','js'], function(){
+//Does all processes and serves from dist + watches
+gulp.task('serve',['sass','html','js','css'], function(){
 	browserSync.init({
 		server:{
 			baseDir: "./dist"
 		}
 	});
-	gulp.watch("src/sass/**/*.scss",['sass']);
+	gulp.watch("src/sass/**/*.scss",['sass','css']);
 	gulp.watch('src/pages/**/*.njk', ['html']);
 	gulp.watch('src/js/*',['js']);
 	gulp.watch('src/images/*'['images']);
 });
 
+
+//process html layouts
 gulp.task('html', function(){
 	return gulp.src('src/pages/html/*.njk')
 		.pipe(nunjucksRender ({
@@ -37,15 +72,17 @@ gulp.task('html', function(){
 		.pipe(reload({stream: true}));
 });
 
+//converts sass to css
 gulp.task('sass' , function(){
 	return gulp.src("src/sass/main.scss")
     	.pipe(sass())
     	.on('error',errorDelete)
-    	.pipe(gulp.dest("dist/css"))
+    	.pipe(gulp.dest("src/css"))
     	.pipe(reload({stream: true}));
 
 });
 
+//minifies images
 gulp.task('image', function(){
 
 	return gulp.src("src/images/*")
@@ -55,6 +92,7 @@ gulp.task('image', function(){
 		.pipe(reload({stream:true}));
 });
 
+//concats and minifies js
 gulp.task('js', function(){
 	return gulp.src('src/js/*.js')
 		.pipe(concat('main.js'))
@@ -64,11 +102,7 @@ gulp.task('js', function(){
 		.pipe(reload({stream:true}));
 });
 
-gulp.task('default', ['serve','image']);
-
-
 //Utility
-
 function errorDelete (error) {
 	console.log(error.toString());
 	this.emit('end')
